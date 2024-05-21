ae52dbd26acce6437c122e1f70d8df53d8e146da
CodeFirstQuestionsLater <codyruggiero@gmail.com>
2024-05-21 09:10:03 -0400

    Added script to run merge.py for all sub-dir automatically

---
+++ merge_all_per_directory.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+# Get the current directory
+current_directory = os.getcwd()
+
+# Iterate over each item in the current directory
+for item in os.listdir(current_directory):
+    item_path = os.path.join(current_directory, item)
+    
+    # Check if the item is a directory
+    if os.path.isdir(item_path):
+        # Construct the command
+        output_file = f"{item}.mp3"
+        command = [
+            "python3", "merge.py",
+            "-s",
+            "-o", output_file,
+            "-d", item_path
+        ]
+        
+        # Execute the command
+        subprocess.run(command)
